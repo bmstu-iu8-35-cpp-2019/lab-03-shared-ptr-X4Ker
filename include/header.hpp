e6cdb1f9de0fe5de67e7ea483e3d12e4954133ed
x4ker <pasha230899@gmail.com>
2020-01-04 22:48:22 +0300

    F

--- include/header.hpp
+++ include/header.hpp
@@ -1,4 +1,4 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Paul Nolan pasha230899@gmail.com
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
@@ -9,139 +9,133 @@
 #include <utility>
 
 class Counter {
-private:
-    std::atomic_int counter;
+ private:
+  std::atomic_int counter;
 
-public:
-    Counter() { counter = 0; }
+ public:
+  Counter() { counter = 0; }
 
-    void inc() { counter++; }
+  void inc() { counter++; }
 
-    void dec() { counter--; }
+  void dec() { counter--; }
 
-    int get() { return counter; }
+  int get() { return counter; }
 
-    ~Counter() = default;
+  ~Counter() = default;
 };
 
-template  <typename T>
+template <typename T>
 class SharedPtr {
-private:
-    T* ptr;
-    Counter* counter;
+ private:
+  T* ptr;
+  Counter* counter;
 
-public:
-    SharedPtr(){
-        ptr = nullptr;
-        counter = nullptr;
-    }
+ public:
+  SharedPtr() {
+    ptr = nullptr;
+    counter = nullptr;
+  }
 
-    SharedPtr(const SharedPtr& other){
-        counter = other.counter;
-        ptr = other.ptr;
-        counter->inc();
-    }
+  SharedPtr(const SharedPtr& other) {
+    counter = other.counter;
+    ptr = other.ptr;
+    counter->inc();
+  }
 
-    SharedPtr(SharedPtr&& other) noexcept {
-        counter = other.counter;
-        ptr = other.ptr;
-        counter->inc();
-    }
+  SharedPtr(SharedPtr&& other) noexcept {
+    counter = other.counter;
+    ptr = other.ptr;
+    counter->inc();
+  }
 
-    ~SharedPtr(){
-        if (*this == true) {
-            counter->dec();
-            if (counter->get() == 0) {
-                delete counter;
-                delete ptr;
-            }
-        }
+  ~SharedPtr() {
+    if (*this == true) {
+      counter->dec();
+      if (counter->get() == 0) {
+        delete counter;
+        delete ptr;
+      }
     }
+  }
 
-    explicit SharedPtr(T* other){
-        counter = new Counter();
-        counter->inc();
-        ptr = other;
-    }
+  explicit SharedPtr(T* other) {
+    counter = new Counter();
+    counter->inc();
+    ptr = other;
+  }
 
-    auto operator=(const SharedPtr& other) -> SharedPtr& {
-        if (*this == true) {
-            counter->dec();
-            if (counter->get() == 0) {
-                delete counter;
-                delete ptr;
-            }
-        }
-        counter = other.counter;
-        ptr = other.ptr;
-        if (*this == true) counter->inc();
-        return *this;
+  auto operator=(const SharedPtr& other) -> SharedPtr& {
+    if (*this == true) {
+      counter->dec();
+      if (counter->get() == 0) {
+        delete counter;
+        delete ptr;
+      }
     }
+    counter = other.counter;
+    ptr = other.ptr;
+    if (*this == true) counter->inc();
+    return *this;
+  }
 
-    auto operator=(SharedPtr&& other) -> SharedPtr&{
-        if (*this == true) {
-            counter->dec();
-            if (counter->get() == 0) {
-                delete counter;
-                delete ptr;
-            }
-        }
-        counter = other.counter;
-        ptr = other.ptr;
-        if (*this == true) counter->inc();
-        return *this;
+  auto operator=(SharedPtr&& other) -> SharedPtr& {
+    if (*this == true) {
+      counter->dec();
+      if (counter->get() == 0) {
+        delete counter;
+        delete ptr;
+      }
     }
+    counter = other.counter;
+    ptr = other.ptr;
+    if (*this == true) counter->inc();
+    return *this;
+  }
 
-    operator bool() const {
-        return counter != nullptr;
-    }
+  operator bool() const { return counter != nullptr; }
 
-    auto operator*() const -> T& {
-        if (*this == false) throw std::invalid_argument("no object");
-        return *ptr;
-    }
+  auto operator*() const -> T& {
+    if (*this == false) throw std::invalid_argument("no object");
+    return *ptr;
+  }
 
-    auto operator-> () const -> T* {
-        return ptr;
-    }
+  auto operator-> () const -> T* { return ptr; }
 
-    auto get() -> T* {
-        return ptr;
-    }
+  auto get() -> T* { return ptr; }
 
-    void reset(){
-        if (*this == true) {
-            counter->dec();
-            if (counter->get() == 0) {
-                delete counter;
-                delete ptr;
-            }
-            counter = nullptr;
-            ptr = nullptr;
-        }
+  void reset() {
+    if (*this == true) {
+      counter->dec();
+      if (counter->get() == 0) {
+        delete counter;
+        delete ptr;
+      }
+      counter = nullptr;
+      ptr = nullptr;
     }
+  }
 
-    void reset(T* other){
-        if (*this == true) {
-            counter->dec();
-            if (counter->get() == 0) {
-                delete counter;
-                delete ptr;
-            }
-            counter = new Counter();
-            counter->inc();
-            ptr = other;
-        }
+  void reset(T* other) {
+    if (*this == true) {
+      counter->dec();
+      if (counter->get() == 0) {
+        delete counter;
+        delete ptr;
+      }
+      counter = new Counter();
+      counter->inc();
+      ptr = other;
     }
+  }
 
-    void swap(SharedPtr& other) {
-        if (ptr != other.ptr) std::swap(other, *this);
-    }
+  void swap(SharedPtr& other) {
+    if (ptr != other.ptr) std::swap(other, *this);
+  }
 
-    auto use_count() const -> size_t {
-        if (!counter) return 0;
-        return static_cast<size_t>(counter->get());
-    }
+  auto use_count() const -> size_t {
+    if (!counter) return 0;
+    return static_cast<size_t>(counter->get());
+  }
 };
 
-#endif // INCLUDE_HEADER_HPP_
+#endif  // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,42 +1,42 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Paul Nolan pasha230899@gmail.com
 
 #include <gtest/gtest.h>
 #include <header.hpp>
 
 TEST(SharedPtr, test) {
-    SharedPtr<int> ptr;
-    EXPECT_FALSE(ptr);
-    EXPECT_EQ(ptr.use_count(), 0);
-    int* temp1 = new int(24);
-    int* temp2 = new int(28);
-    SharedPtr<int> t(temp1);  // - mem
-    EXPECT_EQ(*temp1, *t);
-    EXPECT_TRUE(t);
-    EXPECT_EQ(t.use_count(), 1);
+  SharedPtr<int> ptr;
+  EXPECT_FALSE(ptr);
+  EXPECT_EQ(ptr.use_count(), 0);
+  int* temp1 = new int(24);
+  int* temp2 = new int(28);
+  SharedPtr<int> t(temp1);  // - mem
+  EXPECT_EQ(*temp1, *t);
+  EXPECT_TRUE(t);
+  EXPECT_EQ(t.use_count(), 1);
 
-    SharedPtr<int> t2(t);
-    EXPECT_EQ(t2.use_count(), 2);
-    EXPECT_EQ(*t2, *temp1);
-    t2.reset(temp2);  // - mem
-    EXPECT_EQ(t2.use_count(), 1);
-    EXPECT_EQ(t.use_count(), 1);
-    EXPECT_EQ(*t2, 28);
-    SharedPtr<int> t3(t2);
-    t3.swap(t);
-    EXPECT_EQ(*t, *temp2);
-    EXPECT_EQ(t.use_count(), 2);
-    EXPECT_EQ(*t3, *temp1);
-    EXPECT_EQ(t3.use_count(), 1);
+  SharedPtr<int> t2(t);
+  EXPECT_EQ(t2.use_count(), 2);
+  EXPECT_EQ(*t2, *temp1);
+  t2.reset(temp2);  // - mem
+  EXPECT_EQ(t2.use_count(), 1);
+  EXPECT_EQ(t.use_count(), 1);
+  EXPECT_EQ(*t2, 28);
+  SharedPtr<int> t3(t2);
+  t3.swap(t);
+  EXPECT_EQ(*t, *temp2);
+  EXPECT_EQ(t.use_count(), 2);
+  EXPECT_EQ(*t3, *temp1);
+  EXPECT_EQ(t3.use_count(), 1);
 
-    t2.reset();
+  t2.reset();
 
-    EXPECT_EQ(t.use_count(), 1);
+  EXPECT_EQ(t.use_count(), 1);
 
-    t2 = t3;
-    EXPECT_EQ(t2.use_count(), 2);
-    EXPECT_EQ(t3.use_count(), 2);
-    EXPECT_EQ(t.use_count(), 1);
-    EXPECT_EQ(*t2, *temp1);
-    // delete temp1;
-    // delete temp2;
+  t2 = t3;
+  EXPECT_EQ(t2.use_count(), 2);
+  EXPECT_EQ(t3.use_count(), 2);
+  EXPECT_EQ(t.use_count(), 1);
+  EXPECT_EQ(*t2, *temp1);
+  // delete temp1;
+  // delete temp2;
 }
